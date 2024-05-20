f5bb1fbf5ce3679c53b912ad08abe32f7b808009
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-05-20 06:06:52 +0300

    fix image paths

--- GameWorld/Resources/Utils/Constants.cs
+++ GameWorld/Resources/Utils/Constants.cs
@@ -9,17 +9,17 @@
         public static int TRADEHALL_LIFETIME_IN_DAYS = 7;
         public static int WATER_INTERVAL_IN_HOURS = 1;
         public static int WATER_PER_INTERVAL = 5;
-        public static string CarrotPath = "/Assets/Sprites/Items/carrot.png";
-        public static string CornPath = "/Assets/Sprites/Items/corn.png";
-        public static string WheatPath = "/Assets/Sprites/Items/wheat.png";
-        public static string TomatoPath = "/Assets/Sprites/Items/tomato.png";
-        public static string ChickenPath = "/Assets/Sprites/Items/chicken.png";
-        public static string SheepPath = "/Assets/Sprites/Items/sheep.png";
-        public static string CowPath = "/Assets/Sprites/Items/cow.png";
-        public static string DuckPath = "/Assets/Sprites/Items/duck.png";
-        public static string DuckEggPath = "/Assets/Sprites/Items/duck-egg.png";
-        public static string ChickenEggPath = "/Assets/Sprites/Items/chicken-egg.png";
-        public static string WoolPath = "/Assets/Sprites/Items/wool.png";
-        public static string MilkPath = "/Assets/Sprites/Items/milk.png";
+        public static string CarrotPath = "/Resources/Assets/Sprites/Items/carrot.png";
+        public static string CornPath = "/Resources/Assets/Sprites/Items/corn.png";
+        public static string WheatPath = "/Resources/Assets/Sprites/Items/wheat.png";
+        public static string TomatoPath = "/Resources/Assets/Sprites/Items/tomato.png";
+        public static string ChickenPath = "/Resources/Assets/Sprites/Items/chicken.png";
+        public static string SheepPath = "/Resources/Assets/Sprites/Items/sheep.png";
+        public static string CowPath = "/Resources/Assets/Sprites/Items/cow.png";
+        public static string DuckPath = "/Resources/Assets/Sprites/Items/duck.png";
+        public static string DuckEggPath = "/Resources/Assets/Sprites/Items/duck-egg.png";
+        public static string ChickenEggPath = "/Resources/Assets/Sprites/Items/chicken-egg.png";
+        public static string WoolPath = "/Resources/Assets/Sprites/Items/wool.png";
+        public static string MilkPath = "/Resources/Assets/Sprites/Items/milk.png";
     }
 }
