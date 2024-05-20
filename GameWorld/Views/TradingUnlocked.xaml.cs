8f22c73eaeaf1631ab0c463905871d483591fbd9
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-05-20 06:04:47 +0300

    fix path of images

--- GameWorld/Views/TradingUnlocked.xaml.cs
+++ GameWorld/Views/TradingUnlocked.xaml.cs
@@ -50,8 +50,8 @@ namespace GameWorld.Views
             Get_TextBox.Text = "0";
             getResource = ResourceType.Water;
             giveResource = ResourceType.Water;
-            Give_Button.Source = new BitmapImage(new Uri("/Assets/Sprites/backpack_icon.png", UriKind.Relative));
-            Get_Button.Source = new BitmapImage(new Uri("/Assets/Sprites/backpack_icon.png", UriKind.Relative));
+            Give_Button.Source = new BitmapImage(new Uri("/Resources/Assets/Sprites/backpack_icon.png", UriKind.Relative));
+            Get_Button.Source = new BitmapImage(new Uri("/Resources/Assets/Sprites/backpack_icon.png", UriKind.Relative));
             this.Get_Button.IsEnabled = true;
             this.Give_Button.IsEnabled = true;
         }
@@ -70,7 +70,7 @@ namespace GameWorld.Views
             ResourceType resourceType2 = resource2.ResourceType;
             Get_TextBox.IsReadOnly = true;
             Get_TextBox.Text = trade.ResourceToGetQuantity.ToString();
-            Get_Button.Source = new BitmapImage(new Uri("/Assets/Sprites/backpack_icon.png", UriKind.Relative));
+            Get_Button.Source = new BitmapImage(new Uri("/Resources/Assets/Sprites/backpack_icon.png", UriKind.Relative));
             Get_Button.Source = new BitmapImage(new Uri(tradeService.GetPicturePathByResourceType(resourceType2), UriKind.Relative));
 
             this.Get_Button.IsEnabled = false;
