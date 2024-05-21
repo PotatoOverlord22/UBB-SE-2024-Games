f8a6c35d29cf6bf466fbd45253a863f9f2cbb119
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2024-05-21 22:53:25 +0300

    Table refactoring

--- GameWorld/Entities/Table.cs
+++ GameWorld/Entities/Table.cs
@@ -2,38 +2,29 @@
 {
     public class Table
     {
-        private int tableID;
-        private string tableName;
-        private int tableBuyIn;
-        private int tablePlayerLimit;
-
-        public Table()
+        public Table(Guid tableID, string tableName, int tableBuyIn, int tablePlayerLimit)
         {
+            this.TableID = tableID;
+            this.TableName = tableName;
+            this.TableBuyIn = tableBuyIn;
+            this.TablePlayerLimit = tablePlayerLimit;
         }
 
-        public Table(int tableID, string tableName, int tableBuyIn, int tablePlayerLimit)
+        public Guid TableID
         {
-            this.tableID = tableID;
-            this.tableName = tableName;
-            this.tableBuyIn = tableBuyIn;
-            this.tablePlayerLimit = tablePlayerLimit;
-        }
-
-        public int TableID
-        {
-            get { return tableID; } set { tableID = value; }
+            get; set;
         }
         public string TableName
         {
-            get { return tableName; } set { tableName = value; }
+            get; set;
         }
         public int TableBuyIn
         {
-            get { return tableBuyIn; } set { tableBuyIn = value; }
+            get; set;
         }
         public int TablePlayerLimit
         {
-            get { return tablePlayerLimit; } set { tablePlayerLimit = value; }
+            get; set;
         }
     }
 }
