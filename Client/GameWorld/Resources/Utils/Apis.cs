13e9f9e43719295b6d2cb8777c8cb2ee44d05515
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-05-22 03:17:34 +0300

    add api urls

--- Client/GameWorld/Resources/Utils/Apis.cs
+++ Client/GameWorld/Resources/Utils/Apis.cs
@@ -1,5 +1,6 @@
 ﻿namespace GameWorld.Resources.Utils
 {
+    // Maybe move this to a config file?
     public static class Apis
     {
         public static readonly string ACHIEVEMENTS_BASE_URL = "https://localhost:8080/api/achievements";
