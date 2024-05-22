5db6403061a2afdc7545e2b5f202370424e44fcd
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-05-22 03:16:27 +0300

    update achievement repository to connect to server

---
+++ Client/GameWorld/Resources/Utils/Apis.cs
@@ -0,0 +1,7 @@
+﻿namespace GameWorld.Resources.Utils
+{
+    public static class Apis
+    {
+        public static readonly string ACHIEVEMENTS_BASE_URL = "https://localhost:8080/api/achievements";
+    }
+}
