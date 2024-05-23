3f5b156fbfaa7c231a759f3f3f0b89421cfdb49e
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2024-05-24 02:30:32 +0300

    created MarketSellResourceService and its interface and added it to DependencyInjection

---
+++ Server.API/Server.API/Services/IMarketSellResourceService.cs
@@ -0,0 +1,13 @@
+﻿using Server.API.Models;
+
+namespace Server.API.Services
+{
+    public interface IMarketSellResourceService
+    {
+        Task<List<MarketSellResource>> GetAllMarketSellResourcesAsync();
+        Task<MarketBuyItem> GetMarketSellResourceByResourceIdAsync(Guid resourceId);
+        Task AddMarketSellResourceAsync(IMarketSellResource marketSellResource);
+        Task UpdateMarketSellResourceAsync(MarketSellResource marketSellResource);
+        Task DeleteMarketSellResourceAsync(Guid marketSellResourceId);
+    }
+}
--- Server.API/Server.API/Utils/DependencyInjectionConfiguration.cs
+++ Server.API/Server.API/Utils/DependencyInjectionConfiguration.cs
@@ -14,6 +14,7 @@ namespace Server.API.Utils
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IFarmCellService, FarmCellService>();
             services.AddScoped<ICommentService, CommentService>();
+            services.AddScoped<IMarketSellResourceService, MarketSellResourceService>();
         }
     }
 }
