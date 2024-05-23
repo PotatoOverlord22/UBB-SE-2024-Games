8b16d99902f06615bcd3edcdb66dc27a35344b6d
sorecauadrian <sorecauadrian@gmail.com>
2024-05-23 19:40:43 +0200

    add comment service and interface

---
+++ Server.API/Server.API/Services/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.API.Models;
+
+namespace Server.API.Services
+{
+    public interface ICommentService
+    {
+        Task AddCommentAsync(Comment comment);
+        Task DeleteCommentAsync(Guid id);
+        Task<Comment> GetCommentByIdAsync(Guid id);
+        Task<List<Comment>> GetCommentsAsync();
+        Task UpdateCommentAsync(Guid id, Comment comment);
+    }
+}
