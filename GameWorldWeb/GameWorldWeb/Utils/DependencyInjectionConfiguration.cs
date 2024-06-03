99f37c9b73b814b5e9c6f0682ade1656f68a24e9
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-06-04 01:33:28 +0300

    prepare DI container for ease of use

---
+++ GameWorldWeb/GameWorldWeb/Utils/DependencyInjectionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using GameWorldWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameWorldWeb.Utils
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void ConfigureContexts(IServiceCollection services, ConfigurationManager configuration)
+        {
+            // Add required contexts to the DI Container
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+        }
+        public static void ConfigureRepositories(IServiceCollection services)
+        {
+            // Add required repositories to the DI Container
+
+        }
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Add required services to the DI Container
+
+        }
+    }
+}
